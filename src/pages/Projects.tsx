a799fdd7ad65c70e855a68926d85d030bdd34330
Ted Yao <yaoted00@gmail.com>
2026-05-22 18:28:21 -0600

    jpg support update

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
+import { Globe } from 'lucide-react'
 
 import Adaptive1 from '../assets/AdaptiveWritingAid_Use.png'
 import Adaptive2 from '../assets/AdaptiveWritingAid.png'
 import Adaptive3 from '../assets/AdaptiveWritingAid_CAD.png'
 
-import Exo1 from '../assets/Exo_Jesse.jpg'
-import Exo2 from '../assets/Exo_closeup.jpg'
+import Exo1 from '../assets/Exo_Jesse.JPG'
+import Exo2 from '../assets/Exo_closeup.JPG'
 import Exo3 from '../assets/Exo_ManufacturedParts.png'
 import Exo4 from '../assets/Exo_CAD.png'
 
@@ -18,6 +19,7 @@ interface Project {
   details: string
   images: { src: string; caption?: string }[]
   github?: string
+  website?: string
   live?: string
 }
 
@@ -38,10 +40,11 @@ const projects: Project[] = [
     images: [
       { src: Exo1, caption: 'Full-body view of exoskeleton in operation' },
       { src: Exo2, caption: 'Detailed view of the lower-limb actuation and structural design' },
-      { src: Exo3, caption: 'SolidWorks assembly of right leg of lower-limb exoskeleton' },
-      { src: Exo4, caption: 'Manufactured exoskeleton linkage parts' },
+      { src: Exo3, caption: 'Manufactured exoskeleton linkage parts' },
+      { src: Exo4, caption: 'SolidWorks assembly of right leg of lower-limb exoskeleton' },
     ],
     github: 'https://github.com/McMaster-Exoskeleton',
+    website: 'https://www.macexo.com/'
   },
   {
     id: 2,
@@ -191,6 +194,18 @@ const Projects: React.FC<ProjectsProps> = ({ onNavigate }) => {
                     </svg>
                   </a>
                 )}
+                {project.website && (
+                  <a
+                    href={project.website}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="project-link website-icon"
+                    onClick={(e) => e.stopPropagation()}
+                    aria-label="Open project website"
+                  >
+                    <Globe size={16} strokeWidth={2} />
+                  </a>
+                )}
                 {project.live && (
                   <a 
                     href={project.live} 
@@ -278,6 +293,17 @@ const Projects: React.FC<ProjectsProps> = ({ onNavigate }) => {
                     <svg width="16" height="16" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                       <path fill="currentColor" d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .316.192.686.798.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
                     </svg>
+                  </a>
+                )}
+                {selectedProject.website && (
+                  <a
+                    href={selectedProject.website}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="modal-link website-icon"
+                    aria-label="Open project website"
+                  >
+                    <Globe size={16} strokeWidth={2} />
                   </a>
                 )}
                 {selectedProject.live && (
